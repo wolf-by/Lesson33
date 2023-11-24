1dae6b4b92701ba854043e2d5e27922789a766a1
wolf-by <wolf.by@yandex.ru>
2023-11-24 11:47:02 +0300

    Add total product in Cart

--- public/app.js
+++ public/app.js
@@ -1,13 +1,13 @@
 
 function something()
 		{
-			var x = window.localStorage.getItem('aaa');
+			//var x = window.localStorage.getItem('aaa');
 
-			x = x * 1 + 1;
+			//x = x * 1 + 1;
 
-			window.localStorage.setItem('aaa', x);
+			//window.localStorage.setItem('aaa', x);
 		
-			alert(x);
+			//alert(x);
 		}
 
 function add_to_cart(id)
@@ -16,5 +16,28 @@ function add_to_cart(id)
 			var key = 'product_' + id;
 			var x = window.localStorage.getItem(key);
 			x = x * 1 + 1;
+			
 			window.localStorage.setItem(key, x);
+
+
+			var y = 'Pizza: ' + key + ' Quantity: ' + x + ' Total: ' + cart_get_number_of_items();
+			alert(y);
+
 		}
+
+function cart_get_number_of_items()
+{
+		var cnt = 0;
+
+		for(var i =0; i < window.localStorage.length; i++)
+		{
+			var key = window.localStorage.key(i); //получаем ключ
+			var value = window.localStorage.getItem(key); //получаем значение
+
+			if(key.indexOf('product_') == 0)
+			{
+				cnt = cnt + value *1; 
+			}	
+		}
+		return cnt;
+}
